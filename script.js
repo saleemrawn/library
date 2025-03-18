b2ef6363ef1fd5a3888710abe6a6eac76ba22baf
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-03-18 12:38:52 +0000

    Add addBooksToDOM() to display books in DOM
    Function will loop myLibrary arr and add book obj to library-container element

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ function addBookToLibrary(title = "", author = "", pages = 0, hasRead = false) {
   const book = new Book(title, author, pages, hasRead);
   myLibrary.push(book);
 }
+
+function addBooksToDOM() {
+  const libraryContainer = document.querySelector(".library-container");
+  libraryContainer.innerHTML = "";
+
+  myLibrary.forEach((book) => {
+    libraryContainer.insertAdjacentHTML(
+      "beforeend",
+      `<div class=".card">
+        <p class="book-title">${book.title}</p>
+        <p class="book-author">${book.author}</p>
+        <p class="book-pages">${book.pages}</p>
+        <button class="status-btn">${book.hasRead === false ? "Not Read" : "Read"}</button>
+        <button class="remove-btn">Remove</button>
+      </div>`
+    );
+  });
+}
